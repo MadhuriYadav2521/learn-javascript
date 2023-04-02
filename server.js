faac7c7cec825b50f8f0631ba31c97f65bbfae75
madhuri yadav <madhuri.y.0932@gmail.com>
2023-04-02 18:48:43 +0530

    sakdc

---
+++ server.js
@@ -0,0 +1,44 @@
+// // datatypes
+
+// // var a = 123;
+// // console.log(a);
+// // console.log(typeof(a));
+// // var b = 'abc';
+// // console.log(b);
+// // console.log(typeof(b));
+// // var c = "xyz";
+// // console.log(c);
+// // console.log(typeof(c));
+// // var d = `pqr`;
+// // console.log(d);
+// // console.log(typeof(d));
+// // var e = true
+// // console.log(e);
+// // console.log(typeof(e));
+// // var f = false;
+// // console.log(f);
+// // console.log(typeof(f));
+// // var g;
+// // console.log(g);
+// // console.log(typeof(g));
+// var fname = 'megha';
+// var lname = 'yadav';
+// var name = fname+ ` ` +lname
+// console.log(name);
+// var num1 = 10;
+// var num2 = 20;
+// var add = num1 + num2;
+// console.log(add);
+// var  x ='2344'; 
+// var z = 2344; 
+// console.log(x==z);  //checks value
+// console.log(x===z);  //checks value, datatype
+// console.log(x+z);
+// console.log();
+
+var a = 234;
+var b = 123;
+console.log(b && a);
+//  if a is there print b   , if any one of them value dont have value returns undefined
+console.log(b || a);
+// prints first present value
