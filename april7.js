41da3afa5fa10cb1c8cfb70dccde171c1be395b9
madhuri yadav <madhuri.y.0932@gmail.com>
2023-04-07 12:14:02 +0530

    april 7 update

---
+++ april7.js
@@ -0,0 +1,102 @@
+// var num = 234;
+// if(num % 2 == 0) {
+//     console.log("The number is even.");
+// }
+// else {
+//     console.log("The number is odd.");
+// }
+
+// var num = 234;
+// switch(true){
+//     case num%2==0:
+//         console.log('number is even');
+//         break;
+//     case num%2!=0:
+//         console.log('number is odd');
+//         break;
+// }
+// -----------------------------
+//  var num1 = 5;
+//  var num2 = 10;
+//  if(num1>num2) {
+//      console.log("num1 is large");
+//  }
+//  else {
+//     console.log("num2 is large");
+//  }
+
+
+//  var num1 = 5;
+//  var num2 = 10;
+//  switch(true){
+//      case num1>num2:
+//          console.log('num1 is greater');
+//          break;
+//      case num2>num1:
+//          console.log('num2 is greater');
+//          break;
+//     case num2==num1:
+//         console.log('num is equal');
+//         break;
+//     default:
+//         console.log('invalid number');
+//  }
+
+// --------------------------------------
+
+// var num1 = 15;
+// var num2 = 20;
+// var num3 = 40;
+// var largNum;
+// if(num1 >= num2 && num1 >= num3) {
+//     largNum = num1;
+// }
+// else if (num2 >= num1 && num2 >= num3) {
+//     largNum = num2;
+// }
+// else {
+//     largNum = num3;
+// }
+// console.log("largest number is" + largNum);
+
+
+
+//  var num1 = 15;
+//  var num2 = 20;
+//  var num3 = 40;
+//  switch (true) {
+//      case num1 > num2 && num1 > num3:
+//          console.log('num1 is greater');
+//          break;
+//      case num2 > num1 && num2 > num3:
+//          console.log('num2 is greater');
+//          break;
+//      case num3 > num1 && num3 > num2:
+//          console.log('num3 is greater');
+//          break;
+//  }
+
+// ---------------------------------------------
+// var num = 10;
+// if (num < 5 || num > 25){
+//     console.log('number is not present');
+// }else if (num >= 5 || num <= 25){
+//     console.log('number is  present');
+// }else {
+//     console.log('number is not persent');
+// }
+
+// var num = 10;
+// switch (true) {
+//     case num < 5 || num > 25:
+//         console.log('number is not present');
+//         break;
+//     case num >= 5 || num <= 25:
+//         console.log('number is present');
+//         break;
+
+// }
+
+
+
+
