fae7e01d2dafdb1805951c18b7d1be0673d98e16
madhuri yadav <madhuri.y.0932@gmail.com>
2023-04-08 18:00:25 +0530

    april 7

---
+++ april8.js
@@ -0,0 +1,143 @@
+// function add(){
+//     var a =1;
+//     var b =2;
+//     console.log(a+b);
+// }
+// add();
+// function sub(){
+//     var a =1;
+//     var b =2;
+//     console.log(a-b);
+// }
+// sub();
+
+// var num;
+// function square(num){
+//     return num*num;
+// }
+// console.log(square(2));
+
+// function cube(num){
+//     return num*num*num;
+// }
+
+//  console.log(cube(2));
+
+// var a = 2;
+// var b = 4;
+// function multi(para1, para2) {
+//     console.log(para1 - para2);
+// }
+// multi(a,b);
+
+// var a = 2;
+// var b = 4;
+// function multi(para1, para2) {
+//     return para1 * para2;
+// }
+// var z = multi(a,b);
+// console.log('z =',z);
+
+// var a = 22;
+// var b = 44;
+// function greater(para1, para2) {
+//     if(para1 > para2){
+//         console.log('a ' + para1+' is greater');
+//         return;
+//     }
+//     else{
+//         console.log('b '+ para2+' is greater');
+//         return;
+//     }
+
+// }
+// var z = greater(a,b);
+
+// var a = 2;
+// var b = 4;
+// function greater(a, b) {
+//     var bigger;
+//     if (a> b) {
+//         bigger = a;
+//     }else{
+//         bigger = b;
+//     }
+//     bigger = bigger * 2;
+//     return bigger;
+
+// }
+// var x = greater(a,b)
+// console.log(x);
+
+// var a = 25;
+// function number(a) {
+//     if (a % 2 == 0) {
+//         console.log('even');
+//         return;
+//     } else {
+//         console.log('odd');
+//         return;
+//     }
+// }
+// number(a);
+// --------------
+
+// var num1 = 5;
+// var num2= 10;
+// function greater(a,b) {
+//     if (a>b) {
+//         return a;
+//     }else{
+//         return b
+//     }
+    
+// }
+// var x = greater(num1,num2);
+// console.log(x);
+// ---------------
+
+// var num1 = 885;
+// var num2 = 100;
+// var num3  = 40;
+// function greater(a,b,c) {
+//     if (a>b && a>c) {
+//         return a;
+//     }else if (b> a && b> c) {
+//         return b;
+//     }{
+//         return c;
+//     }
+    
+// }
+// var x = greater(num1,num2,num3);
+// console.log(x);
+// --------------
+
+// var num = 5;
+// function inRange(a) {
+//     if (a >=5 && a <=25) {
+//         return 'number is  in range'
+//     } else {
+//         return 'number is not in range'
+//     }
+    
+// }
+// var x = inRange(num);
+// console.log(x); 
+
+var a;
+var b;
+var c;
+function functionOverload(para1,para2) {
+    return para1 * para2;
+}
+function functionOverload(para1,para2,para3) {
+    return para1 * para2 * para3;
+}
+
+var x = functionOverload(2,4);
+var z = functionOverload(2,4,2);
+console.log(x);
+console.log(z);
+
+
