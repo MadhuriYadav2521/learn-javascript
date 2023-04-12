549fed54891024903a2e6fa1f15ea5a7684193bd
madhuri yadav <madhuri.y.0932@gmail.com>
2023-04-12 12:12:22 +0530

    updates

---
+++ april10-assignment.js
@@ -0,0 +1,8 @@
+//sum of  natural number
+// var sum=0;
+// for( var i=1;i<=10;i++){
+//     sum+=i;
+// }
+// console.log('sum of natural no. = ',sum)
+
+
---
+++ april12.js
@@ -0,0 +1,54 @@
+// var array = [1,2,3,4,5,6,7,8,9,0,11,12,13,14,15];
+// array.push('anu') // add element at last
+// array.pop(); // remove element from last
+// array.shift(); // remove element from start
+// array.shift();
+// array.unshift('megha','mayuri'); //add element at start
+// array.unshift('madhuri');
+// console.log(array,'array');
+// console.log(array.length,'- length of array');
+// console.log(array.length,'array');
+
+
+var studentList = [ 'kunal', 'anu', 'poonam','krishnan','snehal','swaraj']
+// console.log(studentList[2]);
+// console.log(studentList[3]);
+// console.log(studentList[6]);
+
+// console.log(studentList.length);
+// for(var i =0; i<studentList.length;i++){
+//     console.log(studentList[i],'= i');
+// }
+
+// console.log(studentList.length);
+// for(var i =0; i<studentList.length;i++){
+//     if(studentList[i]=='swaraj'){
+//         console.log('kunal is in our data');
+
+//     }
+// }
+
+var flagforSwaraj = false;
+for(var i=0;i<studentList.length;i++){
+    if(studentList[i]=='swaraj'){
+        flagforSwaraj = true;
+        console.log('swaraj is in our data');
+    }
+}
+if(flagforSwaraj == false){
+    console.log('swaraj is not in our db.');
+}
+for(var i=0; i<studentList.length;i++){
+    if(studentList[i]=='swaraj' || studentList[i]=='kunal'){
+        console.log('swaraj and kunal is in our data');
+    }
+}
+
+
+
+
+
+
+
+
+
